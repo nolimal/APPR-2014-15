80f90a9763eee4d0a6d9886b488ef58dd0e88c24
Nolimal Matevž <matevz.nolimal@live.com>
2014-11-27 20:13:15 +0100

    Nova tabela csv ....razpredelnica AktivniSLO

--- uvoz/program za uvoz/uvoz.r
+++ uvoz/program za uvoz/uvoz.r
@@ -9,8 +9,19 @@ uvoziPodatki1 <- function() {
 }
 
 # Zapišimo podatke v razpredelnico ZaposlenostEU.
-cat("Uvažam podatke o zaposlenosti...\n")
+cat("Uvažam podatke o zaposlenosti v EU...\n")
 ZaposlenostEU <- uvoziPodatki1()
+
+
+uvoziPodatki2<-function(){
+  return(read.csv2("slocsv.csv",
+                   header = FALSE, sep = ";", as.is = TRUE,
+                   col.names = c("Regija", "Spol", "Raven izobrazbe", "Aktivni 08", "Delež aktivnih 08", "Aktivni 09", "Delež aktivnih 09", "Aktivni 10", "Delež aktivnih 10", "Aktivni 11", "Delež aktivnih 11", "Aktivni 12", "Delež aktivnih 12", "Aktivni 13", "Delež aktivnih 13"),
+                   na.strings = "-", skip = 3, fileEncoding = "Windows-1250"))
+}
+# Zapišimo podatke v razpredelnico AktivniSLO.
+cat("Uvažam podatke o aktivnih v SLO...\n")
+AktivniSLO <- uvoziPodatki2()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
