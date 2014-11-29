6aa0b4f0df0f4916379f9f754b218ac163658e76
Nolimal Matevž <matevz.nolimal@live.com>
2014-11-29 09:44:58 +0100

    Popravek za direktorij

--- uvoz/program za uvoz/uvoz.r
+++ uvoz/program za uvoz/uvoz.r
@@ -3,7 +3,8 @@
 # Funkcija, ki uvozi podatke iz datoteke geotimesexiscedvecmanj-csv.csv
 uvoziPodatki1 <- function() {
   setwd("C:/Users/matevž/FMF/drugi letnik/APPR-2014-15/uvoz/program za uvoz")
-  
+  d<-dir()
+  z <- grep("\\.csv$", d)
   return(read.csv2("geotimesexiscedvecmanj-csv.csv",
                     header = FALSE, sep = ";", as.is = TRUE,
                     col.names = c("Država", "Spol", paste("Skupno", 2008:2013), "Primerjava 1", paste("Ravni 0-2", 2008:2013),"Primerjava 2",paste("Ravni 3-4", 2008:2013),"Primerjava 3",paste0("Ravni 5-8", 2008:2013),"Primerjava 4"),
@@ -121,6 +122,8 @@ dimnames(povprecje) = list(c("oba spola", "moški","ženske"),c("a2008", "a2009"
 # Funkcija, ki uvozi podatke iz datoteke slocsv.csv
 uvoziPodatki2<-function(){
   setwd("C:/Users/matevž/FMF/drugi letnik/APPR-2014-15/uvoz/program za uvoz")
+  d<-dir()
+  z <- grep("\\.csv$", d)
   return(read.csv2("slocsv.csv",
                    header = FALSE, sep = ";", as.is = TRUE,
                    col.names = c("Regija", "Spol", "Raven izobrazbe", "Aktivni 08", "Delež aktivnih 08", "Aktivni 09", "Delež aktivnih 09", "Aktivni 10", "Delež aktivnih 10", "Aktivni 11", "Delež aktivnih 11", "Aktivni 12", "Delež aktivnih 12", "Aktivni 13", "Delež aktivnih 13"),
