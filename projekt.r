ebaeed7fc95c7c95d0f5234def2dc2a472ae5784
Nolimal Matevž <matevz.nolimal@live.com>
2015-01-03 11:42:06 +0100

    Signed-off-by: Nolimal Matevž <matevz.nolimal@live.com>

--- projekt.r
+++ projekt.r
@@ -3,11 +3,11 @@
 # source("clearpdf.r", encoding = "UTF-8")
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r", encoding="UTF-8")
+
 source("slike/slike.R", encoding="UTF-8")
 
 #3. faza: Analiza in vizualizacija podatkov
 source("vizualizacija/vizualizacija.r",encoding="UTF-8")
-# source("vizualizacija/vizualizacija.r")
 
 # 4. faza: Napredna analiza podatkov
 # source("analiza/analiza.r")
