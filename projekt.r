84e26a925071dc30d5262729dc5417d15d1d1186
Nolimal Matevž <matevz.nolimal@live.com>
2015-01-03 09:44:30 +0100

    Dodana sta 2 grafa o nezaposlenosti in en graf o skupni populaciji v EU.
    Signed-off-by: Nolimal Matevž <matevz.nolimal@live.com>

--- projekt.r
+++ projekt.r
@@ -1,10 +1,13 @@
+# Pobrišemo PDF-je in počistimo delovno okolje
+# silent <- TRUE
+# source("clearpdf.r", encoding = "UTF-8")
 source("clearpdf.r", encoding="UTF-8")
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r", encoding="UTF-8")
 source("slike/slike.R", encoding="UTF-8")
 
 #3. faza: Analiza in vizualizacija podatkov
-
+source("vizualizacija/vizualizacija.r",encoding="UTF-8")
 # source("vizualizacija/vizualizacija.r")
 
 # 4. faza: Napredna analiza podatkov
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -21,10 +21,11 @@ EU<-Evropa[eu,]
 #Znebimo se kolon s samimi NA-ji
 mask <- sapply(EU, function(x) any(is.na(x)))
 EU<-EU[!mask]
-#Naredimo matriko NNezaposlenostEU
+#Naredimo NNezaposlenostEU s spremembo indeksov(potrebna je bila pretvorba v character)
 NNezaposlenostEU <- NezaposlenostEU[as.character(EU$name_long),]
 EU$Skupno13<-NNezaposlenostEU$Skupno13
-
+EU$Moški13<-NNezaposlenostEU$Moški13
+EU$Ženske13<-NNezaposlenostEU$Ženske13
 #Uredim koordinate in imena
 
 koordinate <- coordinates(EU)
@@ -41,13 +42,14 @@ koordinate["Greece",1] <- koordinate["Greece",1]-0.8
 imena["United Kingdom"] <- "United\nKingdom"
 imena["Czech Republic"] <- "Czech\nRepublic"
 
+#PRIMERJAVA MED NEZAPOSLENOSTJO POPULACIJEV V LETU 2013 ZNORAJ EU
 # Narišimo zemljevid v PDF.
-cat("Rišem zemljevid EU...\n")
-pdf("slike/EU.pdf")
+cat("Rišem zemljevid o skupni nezaposlenosti v EU za leto 2013...\n")
+pdf("slike/EUSkupno.pdf")
 
 
 print(spplot(EU, "Skupno13", xlim=c(-25, 40), ylim=c(33, 73),
-             main = "Stopnja nezaposlenosti v EU v letu 2013",
+             main = "Stopnja nezaposlenosti v EU v letu 2013 (v %)",
              col.regions = topo.colors(100),
              sp.layout = list(
                list("sp.polygons", NNezaposlenostEU[,1], fill = "white"),
@@ -56,7 +58,51 @@ print(spplot(EU, "Skupno13", xlim=c(-25, 40), ylim=c(33, 73),
 
 
 dev.off() 
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid o nezaposlenosti moških v EU za leto 2013...\n")
+pdf("slike/EUMoški.pdf")
 
+
+print(spplot(EU, "Moški13", xlim=c(-25, 40), ylim=c(33, 73),
+             main = "Stopnja nezaposlenosti v EU v letu 2013 (v %)",
+             col.regions = topo.colors(100),
+             sp.layout = list(
+               list("sp.polygons", NNezaposlenostEU[,2], fill = "white"),
+               list("sp.text", koordinate, imena, cex = 0.3)),
+             par.settings = list(panel.background=list(col="lightyellow"))))
+
+
+dev.off() 
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid o nezaposlenosti žensk v EU za leto 2013...\n")
+pdf("slike/EUŽenske.pdf")
+
+
+print(spplot(EU, "Ženske13", xlim=c(-25, 40), ylim=c(33, 73),
+             main = "Stopnja nezaposlenosti v EU v letu 2013 (v %)",
+             col.regions = topo.colors(100),
+             sp.layout = list(
+               list("sp.polygons", NNezaposlenostEU[,3], fill = "white"),
+               list("sp.text", koordinate, imena, cex = 0.3)),
+             par.settings = list(panel.background=list(col="lightyellow"))))
+
+
+dev.off()
+
+# Narišimo zemljevid v PDF.
+# Uporabno za predstavo deleža nezaposlenih
+cat("Rišem zemljevid o skupni populaciji v EU za leto 2013...\n")
+koordinate1<-koordinate
+koordinate1[,2]<-koordinate1[,2]+1
+koordinate1["United Kingdom",2] <- koordinate1["United Kingdom",2]+0.5
+koordinate1["Czech Republic",2] <- koordinate1["Czech Republic",2]+0.5
+
+pdf("slike/populacija.pdf")
+plot(EU, xlim=c(-25, 40), ylim=c(33, 73))
+text(koordinate,labels=EU$pop_est,cex = 0.45)
+text(koordinate1,labels=imena,cex = 0.5)
+title("Celotno ocenjeno prebivalstvo v letu 2013")
+dev.off()
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
 # preuredi <- function(podatki, zemljevid) {
 #   nove.regije <- c()
